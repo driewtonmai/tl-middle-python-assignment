7ac841bc02a8a050f1a80c80a507ac001342754d
Tologon Omurzakov <bigbluelamp@gmail.com>
2021-11-22 22:00:57 +0600

    feat: add constant choices for Customer

---
+++ users/constants.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class StatusChoices(models.IntegerChoices):
+    """Status choices for the Customer"""
+
+    INACTIVE = 0, 'Неактивен'
+    ACTIVE = 1, 'Активен'
+
+    __empty__ = 'Выберите статус'
+
+
+class TypeChoices(models.IntegerChoices):
+    """Type choices for the Customer"""
+
+    PRIMARY = 1, 'Первичный'
+    REPEATED = 2, 'Повторный'
+    EXTERNAL = 3, 'Внешний'
+    INDIRECT = 4, 'Косвенный'
+
+    __empty__ = 'Выберите тип'
+
+
+class SexChoices(models.IntegerChoices):
+    """Sex choices for the Customer"""
+
+    MALE = 1, 'Мужской'
+    FEMALE = 2, 'Женский'
+    UNKNOWN = 3, 'Неизвестно'
+
+    __empty__ = 'Выберите пол'
+
