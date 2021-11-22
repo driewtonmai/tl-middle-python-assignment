0b7f74a720abcea4374b5dae643b836ef2a58f3f
Tologon Omurzakov <bigbluelamp@gmail.com>
2021-11-23 00:34:21 +0600

    feat: configure admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+
+from .models import Customer, User, SocialNetwork, Facebook, Vkontakte, AdditionalEmail, AdditionalPhoneNumber
+
+
+admin.site.unregister(Group)
+admin.site.register(AdditionalEmail)
+admin.site.register(AdditionalPhoneNumber)
+
+
+class AdditionalEmailInline(admin.StackedInline):
+    model = AdditionalEmail
+    extra = 0
+
+
+class AdditionalPhoneNumberInline(admin.StackedInline):
+    model = AdditionalPhoneNumber
+    extra = 0
+
+
+class VkontakteInline(NestedStackedInline):
+    model = Vkontakte
+    extra = 0
+    fk_name = 'social_network'
+
+
+class FacebookInline(NestedStackedInline):
+    model = Facebook
+    extra = 0
+    fk_name = 'social_network'
+
+
+class SocialNetworkInline(NestedStackedInline):
+    model = SocialNetwork
+    fk_name = 'customer'
+    inlines = [FacebookInline, VkontakteInline]
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_joined'
+    fields = ('phone', 'email', 'first_name', 'last_name',
+              'last_login', 'date_joined', 'is_active', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'phone')
+    readonly_fields = ('last_login', 'date_joined')
+    list_display = ('phone', 'last_name', 'first_name', 'email')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(is_staff=True)
+
+
+@admin.register(Customer)
+class CustomerAdmin(NestedModelAdmin):
+    date_hierarchy = 'date_joined'
+    fields = ('pub_id', 'phone', 'email', 'first_name', 'last_name',
+              'patronymic', 'status', 'status_changed_date', 'type', 'sex',
+              'timezone', 'last_login', 'date_joined', 'is_active', 'is_staff')
+    readonly_fields = ('pub_id', 'last_login', 'date_joined', 'status_changed_date')
+    inlines = [AdditionalEmailInline, AdditionalPhoneNumberInline, SocialNetworkInline]
+    list_display = ('pub_id', 'last_name', 'first_name', 'patronymic', 'phone', 'email')
+
+
+@admin.register(SocialNetwork)
+class SocialNetworkAdmin(admin.ModelAdmin):
+    inlines = [FacebookInline, VkontakteInline]
